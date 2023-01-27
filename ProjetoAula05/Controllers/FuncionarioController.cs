acd3a68d7e42ec6e3505e6385c51010e8bae24c8
leompb <leompb@gmail.com>
2023-01-26 21:55:22 -0300

    first commit

---
+++ ProjetoAula05/Controllers/FuncionarioController.cs
@@ -0,0 +1,89 @@
+﻿using ProjetoAula05.Entities;
+using ProjetoAula05.Enums;
+using ProjetoAula05.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAula05.Controllers
+{
+    /// <summary>
+    /// Controlador para o fluxo de operações de funcionário
+    /// </summary>
+    public class FuncionarioController
+    {
+        public void CadastrarFuncionario()
+        {
+            try
+            {
+                Console.WriteLine("\n*** CADASTRO DE FUNCIONARIO ***\n");
+
+                var funcionario = new Funcionario();
+                funcionario.Id = Guid.NewGuid();
+
+                Console.Write("ENTRE COM O NOME DO FUNCIONARIO..: ");
+                funcionario.Nome = Console.ReadLine();
+
+                Console.Write("ENTRE COM O CPF DO FUNCIONÁRIO...: ");
+                funcionario.Cpf = Console.ReadLine();
+
+                Console.Write("ENTRE COM A MATRÍCULA DO FUNCIONÁRIO.....: ");
+                funcionario.Matricula = Console.ReadLine();
+
+                Console.Write("ENTRE COM A DATA DE ADMISSAO DO FUNCIONARIO....: ");
+                funcionario.DataAdmissao = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("ENTRE COM O STATUS DO FUNCIONARIO (1,2,3,4)...: ");
+                funcionario.Status = (StatusFuncionario) int.Parse(Console.ReadLine());
+
+                Console.Write("ENTRE COM O ID DA EMPRESA.....: ");
+                funcionario.IdEmpresa = Guid.Parse(Console.ReadLine());
+
+                var funcionarioRepository = new FuncionarioRepository();
+                funcionarioRepository.Inserir(funcionario);
+
+                Console.WriteLine("\nFUNCIONÁRIO CADASTRADO COM SUCESSO!");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("\nErro de validação: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nFalha: " + e.Message);
+            }
+        }
+
+        public void ConsultarFuncionario()
+        {
+            try
+            {
+                Console.WriteLine("\n*** CONSULTA DE FUNCIONÁRIOS ***\n");
+
+                var funcionarioRepository = new FuncionarioRepository();
+                var funcionarios = funcionarioRepository.Consultar();
+
+                foreach (var item in funcionarios)
+                {
+                    Console.WriteLine("ID..............: " + item.Id);
+                    Console.WriteLine("NOME ...: " + item.Nome);
+                    Console.WriteLine("CPF....: " + item.Cpf);
+                    Console.WriteLine("MATRICULA............: " + item.Matricula);
+                    Console.WriteLine("DATAADMISSAO............: " + item.DataAdmissao);
+                    Console.WriteLine("STATUS............: " + item.Status);
+                    Console.WriteLine("IDEMPRESA............: " + item.Empresa.Id);
+                    Console.WriteLine("NOMEFANTASIA.........: " + item.Empresa.NomeFantasia);
+                    Console.WriteLine("RAZAOSOCIAL.........: " + item.Empresa.RazaoSocial);
+                    Console.WriteLine("CNPJ.........: " + item.Empresa.Cnpj);
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nFalha: " + e.Message);
+            }
+        }
+    }
+}
---
+++ ProjetoAula05/Program.cs
@@ -0,0 +1,62 @@
+﻿using ProjetoAula05.Controllers;
+
+namespace ProjetoAula05
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\n*** CONTROLE DE EMPRESAS E FUNCIONÁRIOS ***\n");
+
+            Console.WriteLine("(1) CADASTRAR EMPRESAS");
+            Console.WriteLine("(2) CONSULTAR EMPRESAS");
+            Console.WriteLine("(3) CADASTRAR FUNCIONÁRIO");
+            Console.WriteLine("(4) CONSULTAR FUNCIONARIOS");
+
+            Console.Write("\nENTRE COM A OPÇÃO DESEJADA..: ");
+            var opcao = int.Parse(Console.ReadLine());
+
+            var empresaController = new EmpresaController();
+
+            var funcionarioController = new FuncionarioController();
+
+            switch (opcao)
+            {
+                case 1:
+                    empresaController.CadastrarEmpresa();
+                    break;
+
+                case 2:
+                    empresaController.ConsultarEmpresas();
+                    break;
+
+                case 3:
+                    funcionarioController.CadastrarFuncionario();
+                    break;
+
+                case 4:
+                    funcionarioController.ConsultarFuncionario();
+                    break;
+
+                default:
+                    Console.WriteLine("\nOPÇÃO INVÁLIDA.");
+                    break;
+            }
+
+            Console.Write("\nDeseja Continuar? (S,N)..:");
+            var escolha = Console.ReadLine();
+
+            if(escolha.Equals("S", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.Clear(); //limpar o console
+                Main(args); //recursividade
+
+            }
+            else
+            {
+                Console.WriteLine("\nFim do Programa");
+                Console.ReadKey();
+            }
+        }
+    }
+}
